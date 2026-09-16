2433195d40adeb36f7b105c5d898c39435be1cd1
Thapelosegwe11 <segwe.bz@gmail.com>
2026-09-16 14:15:55 +0200

    Added BasePage.ts

---
+++ utils/BasePage.ts
@@ -0,0 +1,28 @@
+import { Locator, Page, expect } from "@playwright/test";
+
+export class BasPage{
+
+    constructor(public page:Page){
+    }
+
+    async GoTo(url: string){
+        await this.page.goto(url);
+    }
+
+    async ClickElement(locator: Locator){
+        await locator.click();
+    }
+
+    async EnterText(locator: Locator, text: string){
+        await locator.fill(text);
+    }
+
+    async VerifyElementVisible(locator: Locator){
+        await expect(locator).toBeVisible();
+    }
+
+    async GetTextElementValue(locator: Locator){
+        return await locator.textContent();
+    }
+
+}
